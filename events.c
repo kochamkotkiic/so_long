f04d2f7eb529d7209d005331aa394e168c5c5b0b
Emilia Urbanek <emurbane@c1r11s3.42warsaw.pl>
2025-12-05 19:11:41 +0100

    adding explanations

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: emurbane <emurbane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 16:51:59 by emurbane          #+#    #+#             */
-/*   Updated: 2025/12/05 18:31:08 by emurbane         ###   ########.fr       */
+/*   Updated: 2025/12/05 19:11:06 by emurbane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	move_player(t_game *game, int new_x, int new_y)
 		return ;
 	next_tile = game->map[new_y][new_x];
 	if (next_tile == '1' || (next_tile == 'E' && game->collectibles > 0))
-		return ; 
+		return ;
 	if (next_tile == 'C')
 		game->collectibles--;
 	if (next_tile == 'E' && game->collectibles == 0)
@@ -31,10 +31,10 @@ static void	move_player(t_game *game, int new_x, int new_y)
 		close_game(game);
 	}
 	if (game->map[game->p_y][game->p_x] != 'E')
-		game->map[game->p_y][game->p_x] = '0';
+		game->map[game->p_y][game->p_x] = '0';//to gdzie kot stal zamiana na floor
 	game->p_x = new_x;
 	game->p_y = new_y;
-	game->map[new_y][new_x] = 'P';
+	game->map[new_y][new_x] = 'P';//rysujemy kota
 	game->moves++;
 	ft_printf("Moves: %d\n", game->moves);
 	render_map(game);
